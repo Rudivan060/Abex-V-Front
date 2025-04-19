e3638f10180bc14abd6f705535de7ae2aa169fb9
Lucas <lucas_rodriguesguerra@unochapeco.edu.br>
2025-04-18 21:29:16 -0300

    cardapio ajustes

--- pages/cardapio.vue
+++ pages/cardapio.vue
@@ -1,6 +1,6 @@
 <template>
   <v-app>
-    <div class="degrade">
+    <div class="degrade" style="height: 100%;">
       <v-card style="background-color:  #fb911f;">
         <v-tabs v-model="tab" align-tabs="center" color="red" class="custom-tabs-height">
           <a href="./">
@@ -27,8 +27,77 @@
       </v-card>
 
       <div>
+        <v-card
+          color="red"
+          class="centerText centerCard mt-10"
+        >
+          Refrigerantes
+        </v-card>
 
-        <h1>Refrigerantes</h1>
+        <v-row>
+          <v-col 
+            v-for="(item, index) in soda"
+            :key="index"
+            cols="12"
+            md="3"
+          >
+            <v-card
+              class="mx-auto mt-10"
+              max-width="325"
+              elevation="15"
+              style="border-radius: 10px;"
+            >
+              <v-img
+                height="225px"
+                :src="item.imagem"
+                cover
+              />
+      
+              <v-card-title>
+                {{ item.nome }}
+              </v-card-title>
+      
+              <v-card-subtitle>
+                {{ item.descricaoProduto }}
+              </v-card-subtitle>
+      
+              <v-card-actions>
+                <v-btn
+                  color="orange-lighten-2"
+                  text="Requisitar"
+                  @click="openDialog(items.id)"
+                />
+      
+                <v-spacer/>
+      
+                <v-card-text> 
+                  R$ {{ item.valor }}
+                </v-card-text>
+              </v-card-actions>
+            </v-card>
+  
+            <v-dialog v-model="ativo" max-width="500">
+              <v-card height="550" width="500" theme="dark">
+                <v-card-title>
+                  aa {{ produtos.id }}
+                </v-card-title>
+                
+                <v-card-actions>
+                  <v-btn variant="outlined" class="text-none" @click="persist()">
+                    Enviar
+                  </v-btn>
+                </v-card-actions>
+              </v-card>
+            </v-dialog>
+          </v-col>
+        </v-row>
+
+        <v-card
+          color="red"
+          class="centerText centerCard mt-10"
+        >
+          Hambúrguers
+        </v-card>
 
         <v-row>
           <v-col 
@@ -88,8 +157,6 @@
           </v-col>
         </v-row>
       </div>
-
-
     </div>
   </v-app>
 </template>
@@ -106,26 +173,38 @@
         idCategoria:null,
       },
       items: [],
-      teste: [],
+      soda: [],
     }),
 
     async created() {
       await this.getItems();
-      await this.getTeste();
+      await this.getPerCategory();
     },
 
     methods: {
       async getItems() {
         this.loading = true;
         try {
-          const response = await this.$api.get("/produto");
+          const response = await this.$api.get("/produto/categoria/2");
           this.items = response.response;
         } catch (error) {
           console.error("Erro ao carregar itens:", error);
         } finally {
           this.loading = false;
-          console.log("dados carregadosssssssss");
-          console.log(this.items);
+          console.log("dados carregados");
+        }
+      },
+
+      async getPerCategory () {
+        this.loading = true;
+        try {
+          const response = await this.$api.get("/produto/categoria/1");
+          this.soda = response.response;
+        } catch (error) {
+          console.error("Erro ao carregar itens:", error);
+        } finally {
+          this.loading = false;
+          console.log("dados carregados");
         }
       },
 
@@ -154,13 +233,26 @@
     color: darkred;
   }
 
-  .degrade {
-    background: rgb(2, 0, 36);
-    background: linear-gradient(0deg, rgba(2, 0, 36, 1) 0%, rgba(182, 52, 25, 1) 0%, rgba(255, 190, 0, 1) 100%);
-    background-repeat: no-repeat;
-    height: 100vh;
-    min-height: auto;
+  .centerCard {
+    width: 200px;
+    height: 50px;
+    margin: 0 auto;
   }
 
-  
+  .centerText {
+    height: 50px;
+    line-height: 50px;
+    text-align: center;
+    font-size: 26px;
+    font-weight: bolder;
+    color:yellow !important;
+  }
+
+  .degrade {
+    flex: 1; /* Faz com que o degradê ocupe o espaço restante */
+    background: linear-gradient(0deg, rgba(2, 0, 36, 1) 0%, rgba(182, 52, 25, 1) 0%, rgba(255, 190, 0, 1) 100%);
+    background-repeat: no-repeat;
+    background-size: cover;
+  }
+
 </style>
