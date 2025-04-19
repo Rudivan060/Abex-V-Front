49d3a1fc87757e19bb24b6b1439de5f3c4c2ce81
Lucas <lucas_rodriguesguerra@unochapeco.edu.br>
2025-04-18 23:19:40 -0300

    finalização cardápio

--- pages/cardapio.vue
+++ pages/cardapio.vue
@@ -1,164 +1,162 @@
 <template>
-  <v-app>
-    <div class="degrade" style="height: 100%;">
-      <v-card style="background-color:  #fb911f;">
-        <v-tabs v-model="tab" align-tabs="center" color="red" class="custom-tabs-height">
-          <a href="./">
-            <v-tab
-            :value="1" class="text-none custom-tab" style="color: darkred; font-weight: bolder; font-size: larger; margin-right: 70px;">Início</v-tab>
-          </a>  
-    
-          <a href="./cardapio">
-            <v-tab
-            :value="2" class="text-none custom-tab" style="color: darkred; font-weight: bold; font-size: large; margin-right: 70px;">Cardápio</v-tab>
-          </a>
-    
-          <a href="./comanda">
-            <v-tab
-            :value="3" class="text-none custom-tab" style="color: darkred; font-weight: bold; font-size: large; margin-right: 70px;">Comanda</v-tab>
-          </a>
-          
-          <a href="http://localhost:3000/login">
-            <v-icon class="signOut" style="margin-right: 50px;">
-              mdi-account
-            </v-icon>
-          </a>
-        </v-tabs>
+  <div class="degrade" style="height: 100%;">
+    <v-card style="background-color:  #fb911f;">
+      <v-tabs v-model="tab" align-tabs="center" color="red" class="custom-tabs-height">
+        <a href="./">
+          <v-tab
+          :value="1" class="text-none custom-tab" style="color: darkred; font-weight: bolder; font-size: larger; margin-right: 70px;">Início</v-tab>
+        </a>  
+  
+        <a href="./cardapio">
+          <v-tab
+          :value="2" class="text-none custom-tab" style="color: darkred; font-weight: bold; font-size: large; margin-right: 70px;">Cardápio</v-tab>
+        </a>
+  
+        <a href="./comanda">
+          <v-tab
+          :value="3" class="text-none custom-tab" style="color: darkred; font-weight: bold; font-size: large; margin-right: 70px;">Comanda</v-tab>
+        </a>
+        
+        <a href="http://localhost:3000/login">
+          <v-icon class="signOut" style="margin-right: 50px;">
+            mdi-account
+          </v-icon>
+        </a>
+      </v-tabs>
+    </v-card>
+
+    <div>
+      <v-card
+        color="red"
+        class="centerText centerCard mt-10"
+      >
+        Refrigerantes
       </v-card>
 
-      <div>
-        <v-card
-          color="red"
-          class="centerText centerCard mt-10"
+      <v-row>
+        <v-col 
+          v-for="(item, index) in soda"
+          :key="index"
+          cols="12"
+          md="3"
         >
-          Refrigerantes
-        </v-card>
-
-        <v-row>
-          <v-col 
-            v-for="(item, index) in soda"
-            :key="index"
-            cols="12"
-            md="3"
+          <v-card
+            class="mx-auto mt-10"
+            max-width="325"
+            elevation="15"
+            style="border-radius: 10px;"
           >
-            <v-card
-              class="mx-auto mt-10"
-              max-width="325"
-              elevation="15"
-              style="border-radius: 10px;"
-            >
-              <v-img
-                height="225px"
-                :src="item.imagem"
-                cover
+            <v-img
+              height="225px"
+              :src="item.imagem"
+              cover
+            />
+    
+            <v-card-title>
+              {{ item.nome }}
+            </v-card-title>
+    
+            <v-card-subtitle>
+              {{ item.descricaoProduto }}
+            </v-card-subtitle>
+    
+            <v-card-actions>
+              <v-btn
+                color="orange-lighten-2"
+                text="Requisitar"
+                @click="openDialog(items.id)"
               />
-      
+    
+              <v-spacer/>
+    
+              <v-card-text> 
+                R$ {{ item.valor }}
+              </v-card-text>
+            </v-card-actions>
+          </v-card>
+
+          <v-dialog v-model="ativo" max-width="500">
+            <v-card height="550" width="500" theme="dark">
               <v-card-title>
-                {{ item.nome }}
+                aa {{ produtos.id }}
               </v-card-title>
-      
-              <v-card-subtitle>
-                {{ item.descricaoProduto }}
-              </v-card-subtitle>
-      
+              
               <v-card-actions>
-                <v-btn
-                  color="orange-lighten-2"
-                  text="Requisitar"
-                  @click="openDialog(items.id)"
-                />
-      
-                <v-spacer/>
-      
-                <v-card-text> 
-                  R$ {{ item.valor }}
-                </v-card-text>
+                <v-btn variant="outlined" class="text-none" @click="persist()">
+                  Enviar
+                </v-btn>
               </v-card-actions>
             </v-card>
-  
-            <v-dialog v-model="ativo" max-width="500">
-              <v-card height="550" width="500" theme="dark">
-                <v-card-title>
-                  aa {{ produtos.id }}
-                </v-card-title>
-                
-                <v-card-actions>
-                  <v-btn variant="outlined" class="text-none" @click="persist()">
-                    Enviar
-                  </v-btn>
-                </v-card-actions>
-              </v-card>
-            </v-dialog>
-          </v-col>
-        </v-row>
+          </v-dialog>
+        </v-col>
+      </v-row>
 
-        <v-card
-          color="red"
-          class="centerText centerCard mt-10"
+      <v-card
+        color="red"
+        class="centerText centerCard mt-10"
+      >
+        Hambúrguers
+      </v-card>
+
+      <v-row>
+        <v-col 
+          v-for="(item, index) in items"
+          :key="index"
+          cols="12"
+          md="3"
         >
-          Hambúrguers
-        </v-card>
-
-        <v-row>
-          <v-col 
-            v-for="(item, index) in items"
-            :key="index"
-            cols="12"
-            md="3"
+          <v-card
+            class="mx-auto mt-10"
+            max-width="325"
+            elevation="15"
+            style="border-radius: 10px;"
           >
-            <v-card
-              class="mx-auto mt-10"
-              max-width="325"
-              elevation="15"
-              style="border-radius: 10px;"
-            >
-              <v-img
-                height="225px"
-                :src="item.imagem"
-                cover
+            <v-img
+              height="225px"
+              :src="item.imagem"
+              cover
+            />
+    
+            <v-card-title>
+              {{ item.nome }}
+            </v-card-title>
+    
+            <v-card-subtitle>
+              {{ item.descricaoProduto }}
+            </v-card-subtitle>
+    
+            <v-card-actions>
+              <v-btn
+                color="orange-lighten-2"
+                text="Requisitar"
+                @click="openDialog(items.id)"
               />
-      
+    
+              <v-spacer/>
+    
+              <v-card-text> 
+                R$ {{ item.valor }}
+              </v-card-text>
+            </v-card-actions>
+          </v-card>
+
+          <v-dialog v-model="ativo" max-width="500">
+            <v-card height="550" width="500" theme="dark">
               <v-card-title>
-                {{ item.nome }}
+                aa {{ produtos.id }}
               </v-card-title>
-      
-              <v-card-subtitle>
-                {{ item.descricaoProduto }}
-              </v-card-subtitle>
-      
+              
               <v-card-actions>
-                <v-btn
-                  color="orange-lighten-2"
-                  text="Requisitar"
-                  @click="openDialog(items.id)"
-                />
-      
-                <v-spacer/>
-      
-                <v-card-text> 
-                  R$ {{ item.valor }}
-                </v-card-text>
+                <v-btn variant="outlined" class="text-none" @click="persist()">
+                  Enviar
+                </v-btn>
               </v-card-actions>
             </v-card>
-  
-            <v-dialog v-model="ativo" max-width="500">
-              <v-card height="550" width="500" theme="dark">
-                <v-card-title>
-                  aa {{ produtos.id }}
-                </v-card-title>
-                
-                <v-card-actions>
-                  <v-btn variant="outlined" class="text-none" @click="persist()">
-                    Enviar
-                  </v-btn>
-                </v-card-actions>
-              </v-card>
-            </v-dialog>
-          </v-col>
-        </v-row>
-      </div>
+          </v-dialog>
+        </v-col>
+      </v-row>
     </div>
-  </v-app>
+  </div>
 </template>
   
 <script>
@@ -249,10 +247,10 @@
   }
 
   .degrade {
-    flex: 1; /* Faz com que o degradê ocupe o espaço restante */
     background: linear-gradient(0deg, rgba(2, 0, 36, 1) 0%, rgba(182, 52, 25, 1) 0%, rgba(255, 190, 0, 1) 100%);
     background-repeat: no-repeat;
     background-size: cover;
+    height: 100vh;
   }
 
 </style>
