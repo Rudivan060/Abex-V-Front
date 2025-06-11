92c786db40cd094d506aa56fad9a045c127fbb8a
Lucas <lucas_rodriguesguerra@unochapeco.edu.br>
2025-06-10 22:35:51 -0300

    feat: Melhorias em comanda/incompleto

--- pages/comanda.vue
+++ pages/comanda.vue
@@ -1,32 +1,38 @@
 <template>
   <div class="container">
-    <h1>Dolce Fabrizio</h1>
-    <h2>Generador de Comandas</h2>
-    <div class="alert">
-      <p>NO CIERRES AL VENTANA QUE SE PIERDEN LOS DATOS.</p>
-    </div>
+    <h1>Comanda</h1>
     <form id="order-form">
       <div
-        class="coffee-item"
-        v-for="(item, idx) in coffeeItems"
+        v-for="(item, idx) in outFood"
         :key="item.name"
+        class="coffee-item"
       >
         <label>
-          <input type="checkbox" v-model="item.checked">
-          {{ item.name }} (${{ item.price }})
+          {{ item.nome }} - ${{ item.valor }}
         </label>
-        <div class="quantity-controls">
-          <button type="button" class="quantity-btn" @click="decrease(idx)">-</button>
-          <input type="number" min="0" v-model.number="item.quantity" :disabled="!item.checked">
-          <button type="button" class="quantity-btn" @click="increase(idx)">+</button>
+
+        <div style="text-align: right;">
+          <button
+            type="button"
+            class="quantity-btn"
+            :disabled="!item.checked"
+            @click="decrease(idx)"
+          >-</button>
+
+          <input v-model.number="item.quantidade" style="width: 5%; text-align: center;" min="0" :disabled="!item.checked">
+          
+          <button
+            type="button"
+            class="quantity-btn"
+            :disabled="!item.checked"
+            @click="increase(idx)"
+          >+</button>
         </div>
-      </div>
-      <div class="form-actions">
-        <button type="button" @click="generateOrder">Generar Comanda</button>
+
       </div>
     </form>
+
     <div id="order-list">
-      <h2>Listado de Comandas</h2>
       <ul>
         <li v-for="order in orders" :key="order.date">
           <p><strong>{{ order.date }}</strong></p>
@@ -36,15 +42,31 @@
           <p><strong>Total: ${{ order.total }}</strong></p>
         </li>
       </ul>
-      <p>Total vendido hoy: $<span class="total">{{ totalAmount }}</span></p>
+      <p>Total vendido: $<span class="total">{{ totalAmount }}</span></p>
       <p>
         <span v-for="(total, name) in productTotals" :key="name">
-          {{ name }}: ${{ total }}<br />
+          {{ name }}: ${{ total }}<br>
         </span>
       </p>
-    </div>
+    </div><br>
+    
+    <h2>Bah Guri</h2>
+
     <div class="form-actions">
-      <button type="button" @click="exportCSV">Exportar CSV</button>
+      <v-row>
+        <v-col>
+          <v-autocomplete 
+            v-model="token.id"
+            :items="token" 
+            placeholder="Produto"
+            item-title="id" 
+            item-value="id"
+          />
+        </v-col>
+        <v-col>
+          <button type="button" @click="exportCSV">Exportar CSV</button>
+        </v-col>
+      </v-row>
     </div>
   </div>
 </template>
@@ -53,84 +75,112 @@
   export default {
     data() {
       return {
-        coffeeItems: [
-          { name: "Espresso.", price: 10, checked: false, quantity: 0 },
-          { name: "Espresso doble", price: 20, checked: false, quantity: 0 },
-          { name: "Latte", price: 30, checked: false, quantity: 0 },
-          { name: "Capuchino", price: 35, checked: false, quantity: 0 },
-          { name: "Flat white", price: 40, checked: false, quantity: 0 },
-          { name: "Mocha", price: 40, checked: false, quantity: 0 },
-          { name: "Chocolate", price: 40, checked: false, quantity: 0 },
-          { name: "Chai", price: 40, checked: false, quantity: 0 },
-          { name: "Red velvet", price: 40, checked: false, quantity: 0 }
-        ],
+        outFood: [],
+        produto: [],
+        token: [],
         orders: [],
         totalAmount: 0,
         productTotals: {},
       };
     },
+
+    watch: {
+      'token.id'(novoId) {
+        if (novoId) {
+          this.getComanda();
+        }
+      }
+    },
+
+    created() {
+      this.getProduto();
+      this.getComanda();
+      this.getToken();
+    },
+
     methods: {
       increase(idx) {
-        if (this.coffeeItems[idx].checked) this.coffeeItems[idx].quantity++;
+        if (this.outFood[idx].checked) this.outFood[idx].quantity++;
       },
+      
       decrease(idx) {
-        if (this.coffeeItems[idx].checked && this.coffeeItems[idx].quantity > 0)
-          this.coffeeItems[idx].quantity--;
+        if (this.outFood[idx].checked && this.outFood[idx].quantity > 0)
+          this.outFood[idx].quantity--;
       },
-      generateOrder() {
-        const order = {
-          date: new Date().toLocaleString(),
-          items: [],
-          total: 0
-        };
-        this.coffeeItems.forEach((item) => {
-          if (item.checked && item.quantity > 0) {
-            order.items.push({
-              name: item.name,
-              quantity: item.quantity,
-              price: item.price * item.quantity
-            });
-            order.total += item.price * item.quantity;
-            this.productTotals[item.name] =
-              (this.productTotals[item.name] || 0) + item.price * item.quantity;
-          }
-        });
-        if (order.items.length > 0) {
-          this.orders.push(order);
-          this.totalAmount += order.total;
-          // Limpa seleção
-          this.coffeeItems.forEach((item) => {
-            item.checked = false;
-            item.quantity = 0;
+
+      async getComanda() {
+        this.loading = true;
+        try {
+          const response = await this.$api.get(`/pedido/produto/${this.token.id}`);
+          this.outFood = response.response.map((item) => {
+            const produto = this.produto.find((p) => p.id === item.idProduto);
+            return {
+              ...item,
+              nome: produto ? produto.nome : "Desconhecido",
+              valor: produto ? produto.valor : 0,
+            };
           });
+        } catch (error) {
+          console.error("Erro ao carregar itens:", error);
+        } finally {
+          this.loading = false;
+          console.log("dados carregados");
         }
       },
-      exportCSV() {
-        let csvContent = "data:text/csv;charset=utf-8,";
-        csvContent += "Date,Item,Quantity,Price,Total\n";
-        this.orders.forEach((order) => {
-          order.items.forEach((item) => {
-            csvContent += `${order.date},${item.name},${item.quantity},${
-              item.price / item.quantity
-            },${item.price}\n`;
-          });
-          csvContent += `,,,Total,${order.total}\n`;
-        });
-        csvContent += `\nTotal Vendido,,,$,${this.totalAmount}\n`;
-        for (const [product, total] of Object.entries(this.productTotals)) {
-          const item = this.coffeeItems.find((i) => i.name === product);
-          csvContent += `${product},,,${
-            item ? total / item.price : 0
-          },${total}\n`;
+
+      async getProduto() {
+        this.loading = true;
+        try {
+          const response = await this.$api.get("/produto");
+          this.produto = response.response;
+        } catch (error) {
+          console.error("Erro ao carregar itens:", error);
+        } finally {
+          this.loading = false;
+          console.log("dados carregados");
         }
-        const encodedUri = encodeURI(csvContent);
-        const link = document.createElement("a");
-        link.setAttribute("href", encodedUri);
-        link.setAttribute("download", "comandas.csv");
-        document.body.appendChild(link);
-        link.click();
-        document.body.removeChild(link);
-      }
+      },
+
+      async getToken() {
+        this.loading = true;
+        try {
+          const response = await this.$api.get("/token");
+          this.token = response.response;
+        } catch (error) {
+          console.error("Erro ao carregar token:", error);
+        } finally {
+          this.loading = false;
+          console.log('gayy', this.token);
+          console.log("token carregado");
+        }
+      },
+
+      // exportCSV() {
+      //   let csvContent = "data:text/csv;charset=utf-8,";
+      //   csvContent += "Date,Item,Quantity,Price,Total\n";
+      //   this.orders.forEach((order) => {
+      //     order.items.forEach((item) => {
+      //       csvContent += `${order.date},${item.name},${item.quantity},${
+      //         item.price / item.quantity
+      //       },${item.price}\n`;
+      //     });
+      //     csvContent += `,,,Total,${order.total}\n`;
+      //   });
+      //   csvContent += `\nTotal Vendido,,,$,${this.totalAmount}\n`;
+      //   for (const [product, total] of Object.entries(this.productTotals)) {
+      //     const item = this.outFood.find((i) => i.name === product);
+      //     csvContent += `${product},,,${
+      //       item ? total / item.price : 0
+      //     },${total}\n`;
+      //   }
+      //   const encodedUri = encodeURI(csvContent);
+      //   const link = document.createElement("a");
+      //   link.setAttribute("href", encodedUri);
+      //   link.setAttribute("download", "comandas.csv");
+      //   document.body.appendChild(link);
+      //   link.click();
+      //   document.body.removeChild(link);
+      // }
     }
   };
 </script>
@@ -143,7 +193,7 @@
     padding: 0;
     box-sizing: border-box;
   }
-
+  
   .container {
     max-width: 600px;
     margin: 0 auto;
@@ -161,18 +211,12 @@
 
   .coffee-item {
     display: flex;
-    justify-content: space-between;
-    align-items: center;
+    justify-content: space-between; 
     margin-bottom: 15px;
     padding: 10px;
     border-bottom: 1px solid #ddd;
   }
-
-  .quantity-controls {
-    display: flex;
-    align-items: center;
-  }
-
+  
   .quantity-btn {
     background-color: #d7a48f;
     border: none;
